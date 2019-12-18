cd1918878f204bccbc6693c5ff0a9a0e8e7a1404
Budi Yunior Valentino <magenzlizrd@gmail.com>
2019-12-18 20:51:13 +0700

    s

--- Android/CakWangCafe/app/src/main/java/com/ifcodedeveloper/cakwangcafe/Activity/MainActivity.java
+++ Android/CakWangCafe/app/src/main/java/com/ifcodedeveloper/cakwangcafe/Activity/MainActivity.java
@@ -8,8 +8,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
 import com.ifcodedeveloper.cakwangcafe.R;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     ConstraintLayout constraintLayout;
@@ -32,5 +35,22 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, 3000);
+        TimeSet();
+    }
+
+    void TimeSet() {
+        Calendar c = Calendar.getInstance();
+        int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+
+        if (timeOfDay >= 8 && timeOfDay < 17) {
+            Toast.makeText(this, "Shift Pagi", Toast.LENGTH_SHORT).show();
+        } else if (timeOfDay >= 17) {
+            Toast.makeText(this, "Shift Sore", Toast.LENGTH_SHORT).show();
+        } else if (timeOfDay == 0) {
+            Toast.makeText(this, "Shift Sore", Toast.LENGTH_SHORT).show();
+        } else  {
+            Toast.makeText(this, "Cafe Tutup", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
