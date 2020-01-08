5f8cc2a4886c1eace234347cafdeae80b8f8e119
bawik ardiyan <ardiyanramadhan4@gmail.com>
2020-01-08 22:55:56 +0700

    Update hlp.php

--- website/application/views/Laporan/hlp.php
+++ website/application/views/Laporan/hlp.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi =  mysqli_connect("localhost", "root", "", "cakwang");
+$koneksi =  mysqli_connect("localhost", "root", "", "cakwang1");
 $delete  = mysqli_query($koneksi, "DELETE FROM tb_transaksi WHERE id_transasksi= '".$_GET['id']."'");
 if ($delete) {
 	header ('location:laporan_penjualan.php');
